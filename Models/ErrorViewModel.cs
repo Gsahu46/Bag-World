f75667e21327b4af66422fdc770360ad60f2ccbe
Gsahu46 <gsahu4900@gmail.com>
2024-07-20 14:50:11 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bag_World.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
